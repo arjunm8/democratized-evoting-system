32f9d61e17f15aa5dd184fede49e9daea65eba1d
arjunm8 <arjunm1196@gmail.com>
2020-11-03 22:53:12 +0530

    trivial updates

--- client_app_prototype/app/src/main/java/com/technarcs/ccsms/dvoting/login.java
+++ client_app_prototype/app/src/main/java/com/technarcs/ccsms/dvoting/login.java
@@ -30,7 +30,6 @@ public class login extends AppCompatActivity {
         // [START auth_fui_create_intent]
         // Choose authentication providers
         List<AuthUI.IdpConfig> providers = Arrays.asList(
-                new AuthUI.IdpConfig.AnonymousBuilder().build(),
                 new AuthUI.IdpConfig.PhoneBuilder().build());
 
         // Create and launch sign-in intent
@@ -38,6 +37,7 @@ public class login extends AppCompatActivity {
                 AuthUI.getInstance()
                         .createSignInIntentBuilder()
                         .setAvailableProviders(providers)
+                        .setTheme(R.style.AppTheme)
                         .build(),
                 RC_SIGN_IN);
         // [END auth_fui_create_intent]
